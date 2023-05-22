d581abf03c03084353bc5f4f836cc8633d5832df
NikkeshaPillay <u21427900@tuks.co.za>
2023-05-22 21:39:06 +0200

    Estimate Duration Controller Added

---
+++ BOX API/Controllers/EstimateDurationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BOX.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstimateDurationController : ControllerBase
+    {
+    }
+}
