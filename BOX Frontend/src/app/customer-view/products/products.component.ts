0d248ca908b09526205ba29c40bb5afedd015b65
u21430854 <u21430854@tuks.co.za>
2023-07-13 13:54:01 +0200

    UPDATED: -SIZE dropdown on product details page so size options that are out of stock are disabled -You can now see the add to cart form when a product is out of stock but you can't add anything to cart. You can change to different product size in hopes of the size you want being in stock

--- BOX Frontend/src/app/customer-view/products/products.component.ts
+++ BOX Frontend/src/app/customer-view/products/products.component.ts
@@ -72,8 +72,7 @@ export class ProductsComponent {
           categoryID: item.categoryID,
           description: item.description,
           productPhotoB64: foundProd.productPhotoB64,
-          sizeStringArray: [],
-          qtyOnHand: foundProd.quantityOnHand
+          sizeStringArray: []
         }
         this.filteredProductVM.push(prodVM); //populate list that we'll use to display products to the user
       }
