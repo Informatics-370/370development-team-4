20af23a29a047de34813a5fc3107fbfaf6caec92
u21430854 <u21430854@tuks.co.za>
2023-07-18 19:26:19 +0200

    ADDED: -Confirmed total price attribute to estimate entity -Cart service in the frontend
    UPDATED:
    -Estimate controller to CRUD and change an estimate status (backend)
    
    DELETED:
    -Admin FK and confirmed unit price attribute from estimate line entity
    -Estimate line controller

--- BOX API/ViewModel/EstimateLineViewModel.cs
+++ BOX API/ViewModel/EstimateLineViewModel.cs
@@ -1,14 +1,16 @@
 namespace BOX.ViewModel
 {
-  public class EstimateLineViewModel
-  {
-    public int EstimateLineID { get; set; }
-    public int customerID { get; set; }
-    public int EstimateID { get; set; }
-    public int FixedProductID { get; set; }
-    public int AdminID { get; set; }
-    public decimal Confirmed_Unit_Price { get; set; }
-   
+    public class EstimateLineViewModel
+    {
+        public int EstimateLineID { get; set; }
+        public int EstimateID { get; set; }
+        public int FixedProductID { get; set; }
+        public string FixedProductDescription { get; set; } //data that I need when I get a specific estimate
+        public decimal FixedProductUnitPrice { get; set; } //data that I need when I get a specific estimate
+        public int CustomProductID { get; set; }
+        public string CustomProductDescription { get; set; } //data that I need when I get a specific estimate
+        public decimal CustomProductUnitPrice { get; set; } //data that I need when I get a specific estimate
+        public int Quantity { get; set; }
 
-  }
+    }
 }
