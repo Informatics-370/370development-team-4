c76812611701090ee68e9eaf7ca5c8c96b38eb47
Ismail <ismailstarke05@gmail.com>
2023-07-27 13:42:53 +0200

    CRUD Users Backend Coding Complete.

---
+++ BOX API/ViewModel/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BOX.ViewModel
+{
+    public class UserDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Title { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
